cbc33cfab3d1eb2771ace61fbd116449f650b231
Samiunauntor <samiunalimauntor@gmail.com>
2026-02-21 21:11:22 +0600

    created vehicles table

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,6 +7,8 @@ export const pool = new Pool({
 });
 
 const initDB = async () => {
+
+    // create users table
     await pool.query(`CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
         name VARCHAR(100) NOT NULL,
@@ -16,6 +18,18 @@ const initDB = async () => {
         role VARCHAR(20) NOT NULL CHECK(role IN('customer', 'admin'))
     );`
     );
+
+    // create vehicles table
+    await pool.query(`CREATE TABLE IF NOT EXISTS vehicles (
+        id SERIAL PRIMARY KEY,
+        vehicle_name VARCHAR(100) NOT NULL,
+        type VARCHAR(50) NOT NULL CHECK(type IN ('car', 'bike', 'van', 'SUV')),
+        registration_number VARCHAR(50) UNIQUE NOT NULL,
+        daily_rent_price NUMERIC(10, 2) NOT NULL CHECK(daily_rent_price > 0),
+        availability_status VARCHAR(20) NOT NULL CHECK(availability_status IN ('available', 'booked'))
+    );`
+    )
+
 }
 
 
